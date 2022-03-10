f2dd39ff3ab961c0f44e1a5f3fd64f68a12d3826
kerrymdiving <kerry.murphy@mac.com>
2022-03-10 09:50:00 +0000

    practice DB usage

--- Restaurant/src/DB.java
+++ Restaurant/src/DB.java
@@ -10,7 +10,15 @@ public class DB {
         try {
         DB.conn = DriverManager.getConnection(connectionString);
         Statement createTables = DB.conn.createStatement();
-        createTables.execute("CREATE TABLE IF NOT EXISTS restaurants (id INTEGER PRIMARY KEY, name TEXT);");
+        createTables.execute("CREATE TABLE IF NOT EXISTS cafes (id INTEGER PRIMARY KEY, name TEXT);");
+        } catch(SQLException error) {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    public static void close() {
+        try{
+            DB.conn.close();
         } catch(SQLException error) {
             System.out.println(error.getMessage());
         }
